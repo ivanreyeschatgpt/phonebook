df6c7040258cb75bf341571bf64dac5705a8a82a
ivanreyeschatgpt <ivanreyes.chatgpt@gmail.com>
2025-05-31 11:45:45 -0500

    first commit

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ app.get('/api/persons/:id', (request, response) => {
   })
  */
 
-  app.post('/api/persons', (request, response) => {
+  app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
     const person = new Person({
@@ -81,8 +81,41 @@ app.get('/api/persons/:id', (request, response) => {
 
   person.save().then(savedPerson => {
     response.json(savedPerson)
-  })
+  }).catch(error => next(error))
+
 })
+
+
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
+
+
+
+
+
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
